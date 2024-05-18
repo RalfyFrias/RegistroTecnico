1aed3299fcd7d30a9558a6caaa38807f3ec503fa
ralfy <ralfy@RALFY>
2024-05-18 16:46:28 -0500

    actualizacion de la base de datos y hice la union de los modelos

---
+++ RegistroTecnico/Migrations/20240518142444_UnionTecnico-TipoTecnico.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnico.Migrations
+{
+    /// <inheritdoc />
+    public partial class UnionTecnicoTipoTecnico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TipoId",
+                table: "Tecnicos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "TipoTecnico",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoTecnico", x => x.TipoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TipoTecnico");
+
+            migrationBuilder.DropColumn(
+                name: "TipoId",
+                table: "Tecnicos");
+        }
+    }
+}
